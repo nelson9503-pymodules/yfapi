236d158b502c49e240756511025fda47ff1c2464
nelson9503 <nelson9503@hotmail.com>
2021-04-15 17:19:22 +0800

    test

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = "yfapi",
+    packages=[],
+    version='0.0',
+    url = "https://github.com/nelson9503-InvestmentSystem/yfapi",
+    download_url= "https://github.com/nelson9503-InvestmentSystem/yfapi.git"
+)
